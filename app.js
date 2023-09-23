a4c02fa2bf3fc508d9963d9bbfb2ba9a0c519731
Prateek Mishra <pikuaja@gmail.com>
2023-09-23 16:36:45 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -14,8 +14,7 @@ const openai = new OpenAI({
     apiKey: fs.readFileSync("openai-key.txt", "utf8"),
 });
 module.exports = app;
-const policies = fs.readFileSync("output.txt", "utf8");
-
+const context = fs.readFileSync("output.txt", "utf8");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -30,12 +29,8 @@ async function transcribeAudio(filename) {
     return transcript.text;
 }
 
-app.get('/', (req, res) => {
-    const imageFileName = 'logo.jpg'; // Replace with your image file name or path
-    res.render('index.ejs', { imageFileName });
-});
 
-const audio_filename = "audio_recording.mp3";
+const audio_filename = "audio2.mp3";
 
 // Route to receive and process the recorded audio
 app.post('/process-audio', async (req, res) => {
@@ -49,28 +44,27 @@ app.post('/process-audio', async (req, res) => {
     }
 });
 
+const transcribed_text = fs.readFileSync("transcript.txt", "utf8");
+
+async function OrganizeNotes(context, transcribed_text) {
+    const notes = await openai.chat.completions.create({
+        model: "gpt-3.5-turbo",
+        messages: [
+            { role: "system", content: context },
+            { role: "user", content: transcribed_text },
+        ],
+    });
+    console.log(notes.choices[0].message.content);
+    return notes.choices[0].message.content;
+}
+
 // Route to send transcription to GPT for summarization
 app.post('/send-to-gpt', async (req, res) => {
     try {
-        const transcribed_text = req.body.transcript; // Assuming the client sends the transcript in the request body
-        console.log("Sending to GPT:", transcribed_text);
+        // console.log("Sending to GPT:", transcribed_text);
+        const gptResponse = await OrganizeNotes(context, transcribed_text);
 
-        const completion = await openai.chat.completions.create({
-            model: "gpt-3.5-turbo",
-            messages: [
-                { role: "system", content: "You can specify the system message here" },
-                {
-                    role: "user",
-                    content: transcribed_text,
-                },
-            ],
-            stream: true,
-            max_tokens: 150,
-        });
-
-        const gptResponse = completion.choices[0].message.content;
-        console.log("GPT Response:", gptResponse);
-        res.json({ success: true, gptResponse });
+        res.status(200).json({ success: true, gptResponse });
     } catch (error) {
         console.error("Error sending to GPT:", error);
         res.status(400).json({ success: false, error: "GPT request failed" });
--- user-api-test.js
+++ user-api-test.js
@@ -6,25 +6,58 @@ const fs = require('fs');
 chai.use(chaiHttp);
 const expect = chai.expect;
 
-describe('POST /process-audio', function () {
+// describe('POST /process-audio', function () {
+//     this.timeout(10000000);
+//     it('should transcribe audio and return a 200 response', (done) => {
+//         chai.request(app)
+//             .post('/process-audio')
+//             .attach('audio', fs.readFileSync('audio_recording.mp3'), 'audio_recording.mp3') // Update the path accordingly
+//             .end((err, res) => {
+//                 expect(err).to.be.null;
+//                 expect(res).to.have.status(200);
+//                 done();
+//             });
+//     });
+
+//     it('should return a 400 response if no audio file is provided', (done) => {
+//         chai.request(app)
+//             .post('/process-audio')
+//             .end((err, res) => {
+//                 expect(err).to.be.null;
+//                 expect(res).to.have.status(400);
+//                 done();
+//             });
+//     });
+// });
+const context = fs.readFileSync("output.txt", "utf8");
+const transcribed_text = fs.readFileSync("transcript.txt", "utf8");
+
+describe('POST /send-to-gpt', function () {
     this.timeout(10000000);
-    it('should transcribe audio and return a 200 response', (done) => {
+    it('should return a successful response with GPT-generated text', (done) => {
+
+        // Define a mock request body with transcript
+        const requestBody = {
+            model: "gpt-3.5-turbo",
+            messages: [
+                {
+                    role: "system",
+                    content: context
+                },
+                {
+                    role: "user",
+                    content: transcribed_text
+                }
+            ],
+        };
+
         chai.request(app)
-            .post('/process-audio')
-            .attach('audio', fs.readFileSync('audio_recording.mp3'), 'audio_recording.mp3') // Update the path accordingly
+            .post('/send-to-gpt')
+            .send(requestBody)
             .end((err, res) => {
                 expect(err).to.be.null;
                 expect(res).to.have.status(200);
-                done();
-            });
-    });
-
-    it('should return a 400 response if no audio file is provided', (done) => {
-        chai.request(app)
-            .post('/process-audio')
-            .end((err, res) => {
-                expect(err).to.be.null;
-                expect(res).to.have.status(400);
+                expect(res.body).to.have.property('success').to.equal(true);
                 done();
             });
     });
