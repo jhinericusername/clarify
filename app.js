7cdb21b28072983a994907b7797ecebd85429251
Chtun <chtun@live.com>
2023-09-23 18:41:52 -0500

    Streamlined backend

--- app.js
+++ app.js
@@ -18,34 +18,17 @@ const context = fs.readFileSync("output.txt", "utf8");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Transcribe audio
+
 // Transcribe audio
 async function transcribeAudio(filename) {
     const transcript = await openai.audio.transcriptions.create({
         file: fs.createReadStream(filename),
         model: "whisper-1",
     });
-    console.log(transcript.text);
     return transcript.text;
 }
 
-
-const audio_filename = "audio2.mp3";
-
-// Route to receive and process the recorded audio
-app.post('/process-audio', async (req, res) => {
-    try {
-        const transcribed_text = await transcribeAudio(audio_filename);
-        console.log("Transcribed text:", transcribed_text);
-        res.status(200).json({ success: true, transcript: transcribed_text });
-    } catch (error) {
-        console.error("Error processing audio:", error);
-        res.status(400).json({ success: false, error: "Audio processing failed" });
-    }
-});
-
-const transcribed_text = fs.readFileSync("transcript.txt", "utf8");
-
+// Organize notes based on transcript
 async function OrganizeNotes(context, transcribed_text) {
     const notes = await openai.chat.completions.create({
         model: "gpt-3.5-turbo",
@@ -54,20 +37,46 @@ async function OrganizeNotes(context, transcribed_text) {
             { role: "user", content: transcribed_text },
         ],
     });
-    console.log(notes.choices[0].message.content);
     return notes.choices[0].message.content;
 }
 
-// Route to send transcription to GPT for summarization
-app.post('/send-to-gpt', async (req, res) => {
-    try {
-        // console.log("Sending to GPT:", transcribed_text);
-        const gptResponse = await OrganizeNotes(context, transcribed_text);
+// Route to receive and process the recorded audio
+app.post('/process-audio', async (req, res) => {
+    
+    const audio_filename = "audio_recording.mp3";
 
+    // Transcribe the audio and process success or failure events
+    try {
+        const transcribed_text = await transcribeAudio(audio_filename);
+        console.log("Transcribed text:", transcribed_text);
+        fs.writeFile('transcript.txt', transcribed_text, "utf8", (err) => {
+            // In case of a error throw err.
+            if (err) throw err;
+        })
+    } catch (error) {
+        console.error("Error processing audio:", error);
+        res.status(400).json({ success: false, error: "Audio processing failed" });
+    }
+
+    // Organize the notes and process success or failure events
+    try {
+        const transcribed_text = fs.readFileSync("transcript.txt", "utf8");
+        const gptResponse = await OrganizeNotes(context, transcribed_text);
+        console.log("GPT Response: ", gptResponse);
         res.status(200).json({ success: true, gptResponse });
     } catch (error) {
         console.error("Error sending to GPT:", error);
         res.status(400).json({ success: false, error: "GPT request failed" });
+    }
+});
+
+// Send notes and summary text to the frontend
+app.post('/send-notes', async(req, res) => {
+    try {
+        console.log("Sending notes");
+    } catch (error) {
+        console.error("Error sending notes text to Frontend:", error);
+        res.status(400).json({ success: false, error: "Notes request failed" });
     }
 });
 
